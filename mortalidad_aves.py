e34c11569f1e0514d24d5b868776084479c56347
Paradox-Boy <felipe6636@gmail.com>
2024-06-17 23:51:21 -0400

    Add files via upload

--- mortalidad_aves.py
+++ mortalidad_aves.py
@@ -5,6 +5,8 @@
 #grafique cantidad de las siguientes especies: Gaviota garuma, Piquero, Gaviota de Franklin, Pelicano y Guanay (gráfico de barras)
 
 
+import matplotlib.pyplot as plt
+
 def lectura_datos(nombre):
     ent = open(nombre)
     datos = []
@@ -56,24 +58,86 @@ def funcion_b(datos):
         
     return aves_enero2023
 
+
 def funcion_c(datos):
-    pass
+    taguas_muerte = 0
+    for lista in datos:
+        region = regiones_erroneas(lista[2])
+        especie = lista[6].split(" - ")[0]
+        comuna = lista[3]
+        aves_muertas = int(lista[7])
+        if especie.lower() == 'tagua' and comuna.lower() == 'cartagena':
+            taguas_muerte += aves_muertas
+    return taguas_muerte
+
 
 def funcion_d(datos):
-    pass
+    piquero_muerte = 0
+    for lista in datos:
+        fecha = lista[0].split('-')
+        dia = fecha[0]
+        mes = fecha[1]
+        year = fecha[2]
+        aves_muertas = int(lista[7])
+        especie = lista[6].split(" - ")[0]
+        if dia == '12' and mes == '02' and year == '2023' and especie.lower() == 'piquero':
+            piquero_muerte += aves_muertas
+    return piquero_muerte
 
-def funcion_e(especies):
-    pass
+def aves_especie_grafica(datos):
+    g_garuma = 0
+    piquero = 0
+    g_franklin = 0
+    pelicano = 0
+    guanay = 0
+    for lista in datos:    
+        aves_muertas = int(lista[7])
+        especie = lista[6].split(" - ")[0]
+        if especie.lower() == 'gaviota garuma':
+            g_garuma += aves_muertas
+        if especie.lower() == 'piquero':
+            piquero += aves_muertas
+        if especie.lower() == 'gaviota de franklin':
+            g_franklin += aves_muertas
+        if especie.lower() == 'pelicano':
+            pelicano += aves_muertas
+        if especie.lower() == 'guanay':
+            guanay += aves_muertas          
+        else:
+            especie = aves_muertas    
+    return g_garuma, piquero, g_franklin, pelicano, guanay
 
-def salida(region, enero_2023, tagua, piquero):
-    pass
+def funcion_e(valores):
+    especies = ['Gaviota Garuma', 'Piquero', 'Gaviota de Franklin', 'Pelicano', 'Guanay']
+    plt.figure(figsize=(10, 6))
+    plt.bar(especies, valores, color= 'blue')
+    plt.xlabel('Especies')
+    plt.ylabel('Cantidad de Aves Muertas')
+    plt.title('Cantidad de Aves Muertas por Especie')
+    plt.xticks(rotation=360)
+    plt.tight_layout()
+    plt.show()
+
+
+def salida(muertes_region, enero_2023, taguas_muerte, piquero_muerte):
+     sal = open('resultado.txt', 'w', encoding='utf-8')
+     sal.write('Autor(es): M. Ignacia Acevedo Apaz - L. Felipe Bahamondes Garrido \n\n')
+     sal.write('Cantidad de aves muertas por región:\n\n')
+     for region in muertes_region:
+        cantidad = muertes_region[region]
+        sal.write('\t'f'{region}: {cantidad}\n\n')
+     sal.write(f'Casos aves muertas mes enero del año 2023: {enero_2023}\n\n')
+     sal.write(f'En la comuna de Cartagena se detectaron {taguas_muerte} Taguas muertas.\n\n')
+     sal.write(f'Las muertes detectadas para el 12 de febrero del 2023 de la especie Piquero (Sula variegata) son: {piquero_muerte}\n')
+     sal.close()
+
 
 if __name__ == '__main__':
     datos = lectura_datos('aves.txt')
     muertes_region = funcion_a(datos)
-    enero = funcion_b(datos)
-    tagua = funcion_c
-    piquero = funcion_d
-    grafique = funcion_e
-print(muertes_region)
-print(enero)
+    enero_2023 = funcion_b(datos)
+    taguas_muerte = funcion_c(datos)
+    piquero_muerte = funcion_d(datos)
+    valores = aves_especie_grafica(datos)
+    funcion_e(valores)
+    salida(muertes_region, enero_2023, taguas_muerte, piquero_muerte)
